0d4415ea8f4dbe01e266a074e17903390401d355
manurik95 <142829913+manurik95@users.noreply.github.com>
2024-04-30 14:45:27 +0200

    start-prod in playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -12,7 +12,7 @@ const { defineConfig, devices } = require('@playwright/test')
  */
 module.exports = defineConfig({
   webServer: {
-    command: 'npm run start',
+    command: 'npm run start-prod',
     url: 'http://localhost:5000/',
     timeout: 1 * 60 * 1000,
     reuseExistingServer: !process.env.CI
@@ -57,32 +57,5 @@ module.exports = defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] }
     }
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ]
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 })
