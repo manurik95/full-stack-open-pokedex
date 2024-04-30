4c7c03729914c82c54cdf61b5caf234082dae916
manurik95 <142829913+manurik95@users.noreply.github.com>
2024-04-30 08:20:24 +0200

    logger update

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const express = require('express')
 const app = express()
 
+const fs = require('fs')
+const morgan = require('morgan')
+const path = require('path')
+
+const logStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
+  flags: 'a'
+})
+
+app.use(morgan('combined', { stream: logStream }))
+
+const output = fs.createWriteStream(path.join(__dirname, 'server.log'))
+const errorOutput = fs.createWriteStream(
+  path.join(__dirname, 'server-error.log')
+)
+
+// Custom simple logger
+console.log = function (message) {
+  output.write(new Date().toISOString() + ' - ' + message + '\n')
+}
+console.error = function (message) {
+  errorOutput.write(new Date().toISOString() + ' - ' + message + '\n')
+}
+
 // get the port from env variable
 const PORT = 5000
 
